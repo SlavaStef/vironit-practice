0db0a9cf55252fca580d055bcb8ec3543297ed1d
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-03 16:25:37 +0300

    Add global error handler

---
+++ Task4-1/ErrorHandling/ErrorHandling.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace Task4_1.ErrorHandling
+{
+    public static class ErrorHandling
+    {
+        public static void SendEmail(Exception exception)
+        {
+            MailAddress senderEmail = new MailAddress("EMAILNAME@gmail.com", "Tech Support");
+            MailAddress receiverEmail = new MailAddress("EMAILNAME@gmail.com", "Receiver");
+            string password = "PASSWORD";
+            string sub = "Error";
+            string body = exception.ToString();
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(senderEmail.Address, password)
+            };
+            MailMessage message = new MailMessage(senderEmail, receiverEmail)
+            {
+                Subject = sub,
+                Body = body
+            };
+
+            smtp.Send(message);
+        }
+    }
+}
--- Task4-1/Global.asax.cs
+++ Task4-1/Global.asax.cs
@@ -1,48 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using Task4_1.ErrorHandling;
 
 namespace Task4_1
 {
     public class Global : System.Web.HttpApplication
     {
-
-        protected void Application_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Session_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-
-        }
-
         protected void Application_Error(object sender, EventArgs e)
-        {
+        {            
+            Exception exception = Server.GetLastError();
 
-        }
-
-        protected void Session_End(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e)
-        {
-
+            Task4_1.ErrorHandling.ErrorHandling.SendEmail(exception);
+            Response.Redirect("ErrorPage.html");
         }
     }
 }
