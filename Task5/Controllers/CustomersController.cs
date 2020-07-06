30dfc22908c3179980c358a63fbb2c0a3bb710cb
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-06 17:54:47 +0300

    Add controllers and views

---
+++ Task5/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task5.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ActionResult Add()
+        {
+            ViewBag.Message = "Add a new customer";
+            return View();
+        }
+    }
+}
---
+++ Task5/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task5.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult List()
+        {
+            ViewBag.Message = "List of products";
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            ViewBag.Message = "Product Details";
+            return View();
+        }
+    }
+}
