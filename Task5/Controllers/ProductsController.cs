7be7a550ad6026ed57b6a7819264e1de126d31cd
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-07 12:34:56 +0300

    Add server validation

--- Task5/Controllers/ProductsController.cs
+++ Task5/Controllers/ProductsController.cs
@@ -1,27 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Task5.Data;
 using Task5.Models;
 
 namespace Task5.Controllers
 {
     public class ProductsController : Controller
     {
-        List<Product> products = new List<Product>
+        private List<Product> products;
+
+        public ProductsController()
         {
-            new Product { Id = 1, Name = "ASUS ROG Zephyrus G Ultra Slim Gaming Laptop", Description = "15.6” IPS Type FHD, GeForceGTX 1660 Ti, AMD Ryzen 7 3750H, 8GB DDR4, 512GB PCIeNVMe SSD, Windows 10,GA502GU-PB73", Price = 1199
-            },
-            new Product { Id = 2, Name = "Acer Nitro 5 15\" Gaming Laptop", Description = "AMD Ryzen 5 2500U Processor, AMD Radeon RX 560X Graphics, 8GB RAM, 1TB Hard Drive, AN515-42-R5ED", Price = 789
-            },
-            new Product { Id = 3, Name = "Dell G5 Gaming Laptop", Description = "15.6\" Full HD, Intel Core i7-8750H, NVIDIA GeForce GTX 1050 Ti 4GB, 1TB HDD + 128GB SSD Storage, 8GB RAM, G5587-7037RED-PUS", Price = 930
-            },
-            new Product { Id = 4, Name = "Legion By Lenovo Y540", Description = "15.6\" Gaming Laptop, Intel Core i7-9750H, Nvidia GeForce GTX 1650 4GB Graphics, 8GB Memory, 512GB PCIe SSD Storage, 81SY0091US", Price = 1099
-            },
-            new Product { Id = 5, Name = "MSI GL63 Gaming Laptop", Description = "15.6\" Intel Core i7-8750H, NVIDIA GeForce GTX 1050, 8gb RAM, 256gb SSD + 1TB HDD", Price = 1051
-            }
-        };
+            products = Task5.Data.Products.ProductList;
+        }
 
         public ActionResult List()
         {            
@@ -32,6 +27,27 @@ namespace Task5.Controllers
         {
             ViewBag.Message = "Product Details";
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Product product = products.Where(x => x.Id == id).FirstOrDefault();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                int index = products.FindIndex(x => x.Id == product.Id);
+                products[index] = product;
+
+                return RedirectToAction("List");
+            }               
+                            
+            return View(product);
         }
     }
 }
--- Task5/Models/Product.cs
+++ Task5/Models/Product.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Task5.Models
 {
     public class Product
-    {
+    {        
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        [MaxLength(30, ErrorMessage = "Name length is 1 to 30")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price required")]
+        [Range(typeof(decimal), "0,01", "999999,99", ErrorMessage = "Price is more than 0")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Description required")]
+        [MinLength(20, ErrorMessage = "Description length is 20 to 200")]
         public string Description { get; set; }
     }
 }
