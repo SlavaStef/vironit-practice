ba8f71140336bed128c8e0af2a678aaa2ceada54
Slava Stsefanovich <slavastsefanovich@gmail.com>
2020-07-13 17:09:59 +0300

    Add message controller and view

---
+++ Task8/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task8.Models;
+
+namespace Task8.Controllers
+{
+    public class MessageController : Controller
+    {
+        Message[] messages =
+        {
+            new Message { Id = 1, Type = Message.MessageType.Error, Description = "Some error occured", Date = DateTime.Now },
+            new Message { Id = 2, Type = Message.MessageType.Info, Description = "This is an info message", Date = DateTime.Now },
+            new Message { Id = 3, Type = Message.MessageType.Warning, Description = "Warning!!! Warning!!!", Date = DateTime.Now }
+        };
+
+        public ActionResult Index()
+        {
+            Random random = new Random();
+            return View("Message", messages[random.Next(0,2)]);
+        }
+    }
+}
---
+++ Task8/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Task8.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public MessageType Type { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public enum MessageType
+        {
+            Info,
+            Warning,
+            Error
+        }
+    }
+
+    
+}
